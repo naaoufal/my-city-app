802c19eb11268d09c5834ecbb86dc6fda976bdf1
naaoufal <naoufelbenmensour@gmail.com>
2024-03-07 17:10:29 +0100

    second commit

---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomeScreen() {
+    return (
+        <div>
+            <text>this is home screen</text>
+        </div>
+    );
+};
+
+export default HomeScreen;
